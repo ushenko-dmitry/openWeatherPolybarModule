9a9ab3e789e4a14c07a6c90f888fa428059def29
ushenko-dmitry <dimaushenko@mail.ru>
2021-04-05 17:22:53 +0300

    add config file

--- openWeather.py
+++ openWeather.py
@@ -1,11 +1,25 @@
-import requests, json 
+import configparser
 
 from entity.WeatherDTO import WeatherDTO
 from service.WeatherService import WeatherService
 
 city = "Minsk"
 units = "metric"
-lang = "ru"
+lang = "en"
+
+
+
+def setParamsFromConfig():
+    config = configparser.ConfigParser()
+    config.read('config')
+    global city
+    city = config["Global"]["city"]
+    global units
+    units = config["Global"]["units"]
+    global lang
+    lang = config["Global"]["lang"]
+
+setParamsFromConfig()
 
 weatherService = WeatherService()
 
